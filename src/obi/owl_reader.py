456257531206533c94f9f093ed656a2060457db2
Sebastian Duesing <66700705+sebastianduesing@users.noreply.github.com>
2026-03-16 12:09:19 -0400

    Bugfix

--- src/obi/owl_reader.py
+++ src/obi/owl_reader.py
@@ -78,7 +78,7 @@ class FindByLabel(XMLFilterBase):
         super().__init__(parent)
 
     def startElement(self, name, attrs):
-        options = ["owl:Class", "owl:AnnotationProperty", "owl:ObjectProperty", "rdf:Description"]
+        options = ["owl:Class", "owl:AnnotationProperty", "owl:DatatypeProperty", "owl:ObjectProperty", "rdf:Description"]
         parent_options = ["rdfs:subClassOf", "rdfs:subPropertyOf"]
         if name in options:
             if "rdf:about" in attrs.keys():
